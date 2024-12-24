4257a69ce00693aea73be61f8b36a2b5d3fbcf19
Федя Неделькин <fffedosss898@gmail.com>
2024-12-24 21:42:34 +0500

    ыпврап

--- Tyuiu.NedelkinFA.Sprint6.Task4.V7.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint6.Task4.V7.Lib/DataService.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.NedelkinFA.Sprint6.Task4.V7.Lib
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                if (x + 2 == 0) 
+                if (x + 2 == 0)
                 {
                     valueArray[count] = 0;
                 }
