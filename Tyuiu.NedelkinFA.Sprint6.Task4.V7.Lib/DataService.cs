195b5cff4ea4399967322bb246b63b1b1e480794
Федя Неделькин <fffedosss898@gmail.com>
2024-12-24 21:27:19 +0500

    выывыввыыв

--- Tyuiu.NedelkinFA.Sprint6.Task4.V7.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint6.Task4.V7.Lib/DataService.cs
@@ -14,13 +14,13 @@ namespace Tyuiu.NedelkinFA.Sprint6.Task4.V7.Lib
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                if (x == 100 || x + 2 == 0)
+                if (x + 2 == 0)  
                 {
                     valueArray[count] = 0;
                 }
                 else
                 {
-                    y = Math.Round(Math.Cos(x) / (x + 2), 2);
+                    y = Math.Round(Math.Cos(x) / (x + 2), 2) - 3 * x;
                     valueArray[count] = y;
                 }
                 count++;
