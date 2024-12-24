48a7cfaeb97d5954930c1fdc23e0f87f1c3616f4
Федя Неделькин <fffedosss898@gmail.com>
2024-12-24 21:52:50 +0500

    ап

---
+++ Tyuiu.NedelkinFA.Sprint6.Task6.V3/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.NedelkinFA.Sprint6.Task6.V3
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
