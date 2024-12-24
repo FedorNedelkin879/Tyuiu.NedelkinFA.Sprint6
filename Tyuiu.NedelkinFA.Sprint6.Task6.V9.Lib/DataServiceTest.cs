f43c9bc68832b3efb91d200738647ec67e2dea4f
Федя Неделькин <fffedosss898@gmail.com>
2024-12-24 22:16:53 +0500

    фыыффы

---
+++ Tyuiu.NedelkinFA.Sprint6.Task6.V9.Lib/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.NedelkinFA.Sprint6.Task6.V9.Lib
+{
+    public class DataServiceTest : ISprint6Task6V9
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string resStr = "";
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var temp = line.Split(' ');
+                    resStr += temp[^1] + " ";
+                }
+                return resStr.Trim();
+            }
+        }
+    }
+}
