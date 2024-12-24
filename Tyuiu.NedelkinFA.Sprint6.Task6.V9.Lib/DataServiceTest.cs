8b76efc58c97555ec789c36e9022f0669b99d9db
Федя Неделькин <fffedosss898@gmail.com>
2024-12-24 22:24:35 +0500

    фы

--- Tyuiu.NedelkinFA.Sprint6.Task6.V9.Lib/DataServiceTest.cs
+++ Tyuiu.NedelkinFA.Sprint6.Task6.V9.Lib/DataServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using Microsoft.Win32;
 using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.NedelkinFA.Sprint6.Task6.V9.Lib
@@ -16,10 +17,28 @@ namespace Tyuiu.NedelkinFA.Sprint6.Task6.V9.Lib
                 while ((line = reader.ReadLine()) != null)
                 {
                     var temp = line.Split(' ');
+
                     resStr += temp[^1] + " ";
                 }
                 return resStr.Trim();
             }
+        }
+
+        public string LoadFileToTextBox()
+        {
+            string filePath = "";
+
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath = openFileDialog.FileName;
+            }
+
+            return CollectTextFromFile(filePath);
         }
     }
 }
