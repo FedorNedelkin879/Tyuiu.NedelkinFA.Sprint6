1bfcfda3c2ac1238965dba0b30d2520ec19562e6
Федя Неделькин <fffedosss898@gmail.com>
2024-12-24 20:11:45 +0500

    dsf

---
+++ Tyuiu.NedelkinFA.Sprint6.Task2.V15.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.NedelkinFA.Sprint6.Task2.V15.Lib
+{
+    public class DataService : ISprint6Task2V15
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] valueArray = new double[len];
+            int count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double denominator = x + 1.2;
+                double y;
+
+                if (denominator == 0)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y = Math.Round(
+                        (2 * Math.Sin(x) / denominator) +
+                        Math.Cos(x) -
+                        (7 * x * 2),
+                        2
+                    );
+                }
+
+                valueArray[count] = y;
+                count++;
+            }
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.NedelkinFA.Sprint6.Task2.V15/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.NedelkinFA.Sprint6.Task2.V15
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
