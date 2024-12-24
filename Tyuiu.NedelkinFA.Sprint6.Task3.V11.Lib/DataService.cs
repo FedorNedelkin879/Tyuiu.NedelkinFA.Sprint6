89bad10b8c570ea64e1ce561d9406720b7eb5979
Федя Неделькин <fffedosss898@gmail.com>
2024-12-24 20:43:12 +0500

    ыв

---
+++ Tyuiu.NedelkinFA.Sprint6.Task3.V11.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.NedelkinFA.Sprint6.Task3.V11.Lib
+{
+    public class DataService : ISprint6Task3V11
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            int[] firstColumn = new int[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                firstColumn[i] = matrix[i, 0];
+            }
+            Array.Sort(firstColumn);
+
+            for (int i = 0; i < rows; i++)
+            {
+                matrix[i, 0] = firstColumn[i];
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.NedelkinFA.Sprint6.Task3.V11/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.NedelkinFA.Sprint6.Task3.V11
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
