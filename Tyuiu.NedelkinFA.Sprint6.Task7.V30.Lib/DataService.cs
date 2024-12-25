276a9754bb3779dbec6634e3979edcbc9bf15f79
Федя Неделькин <fffedosss898@gmail.com>
2024-12-25 22:12:06 +0500

    ыф

---
+++ Tyuiu.NedelkinFA.Sprint6.Task7.V30.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.NedelkinFA.Sprint6.Task7.V30.Lib
+{
+    public class DataService : ISprint6Task7V30
+    {
+        public int[,] GetMatrix(string path)
+        {
+            string[] file = File.ReadAllLines(path);
+            int[,] matrix = new int[file.Length, file[0].Split(';').Length];
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                string[] line = file[i].Split(';');
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(line[j]);
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.NedelkinFA.Sprint6.Task7.V30.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.NedelkinFA.Sprint6.Task7.V30.Lib;
+
+namespace Tyuiu.NedelkinFA.Sprint6.Task7.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
